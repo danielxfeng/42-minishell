adf5d74ba1665b30722fc593ccdd773edc0dde82
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-02-07 14:28:47 +0200

    norm.

--- src/executor/cmd_handler.c
+++ src/executor/cmd_handler.c
@@ -6,24 +6,23 @@
 /*   By: Xifeng <xifeng@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 21:16:05 by Xifeng            #+#    #+#             */
-/*   Updated: 2025/02/06 21:16:06 by Xifeng           ###   ########.fr       */
+/*   Updated: 2025/02/07 14:27:23 by Xifeng           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/executor.h"
 #include "../libs/libft/libft.h"
-#include <sys/wait.h>
 #include <errno.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-bool is_parse_free_cmd(char *cmd);
-bool	is_empty_cmd(char *cmd);
-char **get_path();
-void free_path(char ***path);
-char *ms_strjoin(t_ast *ast, char *path, char *cmd);
-
+bool		is_parse_free_cmd(char *cmd);
+bool		is_empty_cmd(char *cmd);
+char		**get_path(void);
+void		free_path(char ***path);
+char		*ms_strjoin(t_ast *ast, char *path, char *cmd);
 
 // @brief check if the full cmd is valid.
 //
@@ -32,28 +31,31 @@ char *ms_strjoin(t_ast *ast, char *path, char *cmd);
 // @return if the cmd is valid.
 static int	check_cmd(t_ast *ast, t_cmd_prop *prop)
 {
-	struct stat buf;
-	
+	struct stat	buf;
+
 	if (access(prop->full_cmd, F_OK) < 0)
-		return (return_with_err(INVALID_ERR_NO, EXIT_CMD_ERR, ast->tokens[prop->start]));
+		return (return_with_err(INVALID_ERR_NO, EXIT_CMD_ERR,
+				ast->tokens[prop->start]));
 	if (access(prop->full_cmd, X_OK) < 0)
-		return (return_with_err(INVALID_ERR_NO, EXIT_EXEC_ERR, ast->tokens[prop->start]));
+		return (return_with_err(INVALID_ERR_NO, EXIT_EXEC_ERR,
+				ast->tokens[prop->start]));
 	stat(prop->full_cmd, &buf);
 	if (S_ISDIR(buf.st_mode))
-		return (return_with_err(EISDIR, EXIT_EXEC_ERR, ast->tokens[prop->start]));
+		return (return_with_err(EISDIR, EXIT_EXEC_ERR,
+				ast->tokens[prop->start]));
 	return (true);
 }
 
 // @brief try to parse the full cmd from env.
-// 
+//
 // @param ast: the pointer to ast tree.
 // @param prop: the pointer to property of node.
 // @return if there is existing file.
-static bool parse_full_cmd(t_ast *ast, t_cmd_prop *prop)
+static bool	parse_full_cmd(t_ast *ast, t_cmd_prop *prop)
 {
-	int i;
-	char **path;
-	char *full_cmd;
+	int		i;
+	char	**path;
+	char	*full_cmd;
 
 	i = 0;
 	path = get_path();
@@ -80,17 +82,18 @@ static bool parse_full_cmd(t_ast *ast, t_cmd_prop *prop)
 // @param ast: the pointer to ast.
 // @param prop: the property of `cmd` node.
 // @return the status.
-static int parse_full_cmd_and_check(t_ast *ast, t_cmd_prop *prop)
+static int	parse_full_cmd_and_check(t_ast *ast, t_cmd_prop *prop)
 {
-	if (!(is_parse_free_cmd(ast->tokens[prop->start])) ||
-	 !(parse_full_cmd(ast, prop)))
-		return (return_with_err(ENOENT, EXIT_CMD_ERR, ast->tokens[prop->start]));
+	if (!(is_parse_free_cmd(ast->tokens[prop->start])) || !(parse_full_cmd(ast,
+				prop)))
+		return (return_with_err(ENOENT, EXIT_CMD_ERR,
+				ast->tokens[prop->start]));
 	return (check_cmd(ast, prop));
 }
 
-static void generate_argv(t_ast *ast, t_cmd_prop *prop)
+static void	generate_argv(t_ast *ast, t_cmd_prop *prop)
 {
-	int i;
+	int	i;
 
 	prop->argv = ft_calloc(prop->size + 1, sizeof(char *));
 	if (!(prop->argv))
@@ -120,7 +123,8 @@ int	cmd_handler(t_ast *ast, t_ast_node *ast_node)
 	if (ast_node->left)
 		ast_node->left->node_handler(ast, ast_node->left);
 	if (is_empty_cmd(ast->tokens[prop->start]))
-		return (return_with_err(ENOENT, EXIT_CMD_ERR, ast->tokens[prop->start]));
+		return (return_with_err(ENOENT, EXIT_CMD_ERR,
+				ast->tokens[prop->start]));
 	if (is_builtin_func(ast->tokens[prop->start]))
 		return (exec_builtin_func(ast->tokens, prop->start, prop->size));
 	status = parse_full_cmd_and_check(ast, prop);
