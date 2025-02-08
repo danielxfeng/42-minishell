e69820487d91b1e0529f1f1898d3e9a49a5a4adb
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-02-08 08:39:13 +0000

    updated built-in exit.

--- src/builtin/builtin_funcs.c
+++ src/builtin/builtin_funcs.c
@@ -6,12 +6,14 @@
 /*   By: Xifeng <xifeng@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 10:25:11 by Xifeng            #+#    #+#             */
-/*   Updated: 2025/02/07 18:51:51 by Xifeng           ###   ########.fr       */
+/*   Updated: 2025/02/08 08:37:59 by Xifeng           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../libs/libft/libft.h"
 #include "../include/executor.h"
 #include <stdlib.h>
+#include <errno.h>
 
 // @brief `echo` with option `-n`
 int	cmd_echo(t_ast *ast, t_cmd_prop *prop)
@@ -22,7 +24,12 @@ int	cmd_echo(t_ast *ast, t_cmd_prop *prop)
 // @brief `cd` with only a relative or absolute path
 int	cmd_cd(t_ast *ast, t_cmd_prop *prop)
 {
-	return (0);
+	if (prop->size != 1)
+		return_prt_err(EXIT_FAIL, "minishell: exit: too many arguments\n");
+	
+	if (chdir(ast->tokens[prop->start + 1]) != 0)
+		return_with_err(INVALID_ERR_NO, EXIT_FAIL, "");
+	return (EXIT_OK);
 }
 
 // @brief `pwd` with no options
@@ -31,7 +38,7 @@ int	cmd_pwd(t_ast *ast, t_cmd_prop *prop)
 	char *home;
 
     if (prop->size != 1)
-        return_with_err(?, EXIT_FAIL, "minishell: pwd: ");
+	return_prt_err(EXIT_FAIL, "minishell: exit: too many arguments\n");
 	home = getenv("HOME");
 	printf("%s\n", home);
 }
@@ -57,8 +64,24 @@ int	cmd_env(t_ast *ast, t_cmd_prop *prop)
 // @brief `exit` with no options
 int	cmd_exit(t_ast *ast, t_cmd_prop *prop)
 {
-    if (prop->size != 1)
-        return_with_err(?, EXIT_FAIL, "minishell: exit: ");
-    close_ast(&ast);
-    exit(EXIT_OK);
+	int status;
+	
+    if (prop->size != 1 && prop->size != 2)
+        return_prt_err(EXIT_FAIL, "minishell", "exit", "too many arguments");
+	status = EXIT_OK;
+	if (prop->size == 2 && (ft_strcmp(ast->tokens[prop->start + 1], "0") != 0 &&
+		ft_strcmp(ast->tokens[prop->start + 1], "+0") != 0 && 
+		ft_strcmp(ast->tokens[prop->start + 1], "-0") != 0))
+	{
+		status = ft_atoi(ast->tokens[prop->start + 1]);
+		if (status == 0)
+		{
+			ft_putstr_fd("minishell: exit: ", 2);
+			ft_putstr_fd(ast->tokens[prop->start + 1], 2);
+			ft_putstr_fd(": numeric argument required\n", 2);
+			status = EXIT_FAIL;
+		}
+	}
+	close_ast(&ast);
+    exit(status);
 }
