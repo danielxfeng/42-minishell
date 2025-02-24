118b33a4577dc1a76f73168f5ff8bbafe7d7de08
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-02-24 13:53:45 +0200

    norm

--- src/executor/cmd_handler_supp2.c
+++ src/executor/cmd_handler_supp2.c
@@ -6,7 +6,7 @@
 /*   By: Xifeng <xifeng@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 07:50:41 by Xifeng            #+#    #+#             */
-/*   Updated: 2025/02/23 18:13:47 by Xifeng           ###   ########.fr       */
+/*   Updated: 2025/02/24 13:52:01 by Xifeng           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,7 @@ static int	check_cmd(t_ast *ast, t_cmd_prop *prop)
 
 	if (access(prop->full_cmd, F_OK) < 0)
 		return (return_prt_err(EXIT_CMD_ERR, "minishell",
-			ast->tokens[prop->start], NULL));	
+				ast->tokens[prop->start], NULL));
 	if (access(prop->full_cmd, R_OK) < 0 || access(prop->full_cmd, X_OK) < 0)
 		return (return_prt_err(EXIT_EXEC_ERR, "minishell",
 				ast->tokens[prop->start], NULL));
